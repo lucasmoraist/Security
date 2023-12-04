72c2c86d9e7f96750915cd5875d06981def38f59
lucasmoraist <luksmnt1101@gmail.com>
2023-12-04 08:14:05 -0300

    Esqueleto do projeto

---
+++ src/main/java/com/lucas/accesssync/AccessSyncApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.accesssync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessSyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucas/accesssync/AccessSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.accesssync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
